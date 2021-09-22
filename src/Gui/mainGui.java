225dd5b2dae0ed1769f20d7c09431a0fd636f7db
Sebastian Morales <54720084+JuanSebastianMoralesVilla@users.noreply.github.com>
2021-09-22 08:12:57 -0500

    add interfaz

--- src/Gui/mainGui.java
+++ src/Gui/mainGui.java
@@ -1,5 +1,27 @@
 package Gui;
 
-public class mainGui {
+import java.io.IOException;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class mainGui extends Application {
+
+	public static void main(String[] args) {
+		launch();
+	}
+
+	@Override
+	public void start(Stage stage) throws IOException {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("interfaz.fxml"));
+		Parent root = loader.load();
+		Scene scene = new Scene(root);
+		stage.setTitle("Automata");
+		stage.setScene(scene);
+	
+		stage.show();
+	}
 }
