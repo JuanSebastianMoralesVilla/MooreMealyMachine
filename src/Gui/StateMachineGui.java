3bea435f584e8d9eef43f6812ac733114049c502
Sebastian Morales <54720084+JuanSebastianMoralesVilla@users.noreply.github.com>
2021-10-02 12:59:15 -0500

    add gui controller

---
+++ src/Gui/StateMachineGui.java
@@ -0,0 +1,41 @@
+package Gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+
+
+
+public class StateMachineGui {
+	
+
+
+	    @FXML
+	    private TextField txtEstados;
+
+	    @FXML
+	    private TextField txtAlfabeto;
+
+	    @FXML
+	    private RadioButton tgMealy;
+
+	    @FXML
+	    private ToggleGroup machine;
+
+	    @FXML
+	    private RadioButton tgMoore;
+
+	    @FXML
+	    private Button btCrearTabla;
+
+	    @FXML
+	    private Button btMin;
+	    
+	    
+	    
+
+	
+
+}
