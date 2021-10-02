fd899bd7ba792a80995af1165277e651c6d8b1a6
Alexander <alexssjr2002@gmail.com>
2021-10-02 16:36:32 -0500

    modifed

--- src/model/MealyMachine.java
+++ src/model/MealyMachine.java
@@ -7,6 +7,14 @@ package model;
  * @author sebastianmorales
  *
  */
-public class MealyMachine {
+public class MealyMachine extends Automaton{
+
+    private int numberOfStates;
+    private String alphabet;
+
+    public MealyMachine(int numberOfStates, String alphabet) {
+        super(numberOfStates, alphabet);
+    }
+
 
 }
